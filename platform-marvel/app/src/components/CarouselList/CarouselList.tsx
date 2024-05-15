29cbc776612cabb5f08d716ca71c5823974cd81c
igregorioneto <igregoriodoneto@outlook.com>
2024-05-15 17:18:45 -0300

    feat: Troca do Carousol por List

--- platform-marvel/app/src/components/CarouselList/CarouselList.tsx
+++ platform-marvel/app/src/components/CarouselList/CarouselList.tsx
@@ -1,34 +1,52 @@
-import { Carousel } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
 import { CardList } from "../CardList/CardList";
+import { useState } from "react";
 
 interface CaroulselListProps {
     propList: any[];
     images: { [key: string]: string };
 }
 
-export const CarouselList: React.FC<CaroulselListProps> = ({ propList, images }) => (
-    <div className="row justify-content-center">
-        <div className="col-md-12" style={{ width: '100vw' }}>
-            <Carousel
-                interval={null}
-                prevIcon={null}
-                prevLabel={null}
-                indicators={null}
-            >
-                {propList.map((props) => (
-                    <Carousel.Item key={props.id}>
-                        <div className="d-flex justify-content-center">
-                            <CardList
-                                name={props.name}
-                                description={props.description}
-                                backgroundImage={images[props.image_id] || ""}
-                                info={props.streaming_platform}
-                                avaliations={props.critic_rating}
-                            />
-                        </div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
+export const CarouselList: React.FC<CaroulselListProps> = ({ propList, images }) => {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex: number) => (prevIndex + 1) % propList.length);
+    }
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex: number) => (prevIndex - 1 + propList.length) % propList.length);
+    }
+
+    return (
+        <div className="row justify-content-center">
+            <div className="col-md-12" style={{ width: '100vw' }}>
+
+                <div style={{ display: 'flex', alignItems: 'center', overflowX: 'hidden' }}>
+                    <Button onClick={handlePrev}
+                        style={{ backgroundColor: '#ff0000', marginRight: '10px', display: currentIndex === 0 ? 'none' : 'block' }}
+                    >{'<'}</Button>
+                    <div style={{ width: '100%', maxWidth: '80vw', overflowX: 'hidden' }}>
+                        <ListGroup horizontal style={{ margin: '0', padding: '0', whiteSpace: 'nowrap' }}>
+                            {propList.map((props, index) => (
+                                <ListGroup.Item key={props.id} style={{ background: 'none', border: 'none', flex: '0 0 auto', width: '80vw', maxWidth: '140px', margin: '0 10px', transform: `translateX(${(index - currentIndex) * 100}%)`, transition: 'transform 0.5s ease-in-out' }}>
+                                    <CardList
+                                        name={props.name}
+                                        description={props.description}
+                                        backgroundImage={images[props.image_id] || ""}
+                                        info={props.streaming_platform}
+                                        avaliations={props.critic_rating}
+                                    />
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </div>
+                    <Button onClick={handleNext}
+                        style={{ backgroundColor: '#ff0000', marginLeft: '10px', display: currentIndex === propList.length - 1 ? 'none' : 'block' }}
+                    >{'>'}</Button>
+                </div>
+
+            </div>
         </div>
-    </div>
-);
+    );
+}
