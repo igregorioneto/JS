2e1f1fe122c1e42eddec48bc2e400192a29aefd0
igregorioneto <igregoriodoneto@outlook.com>
2024-05-27 20:58:18 -0300

    feat: Acrescentando Rotas de Testes de Get e Post

--- js-node/testing/e2e/src/todo/api.js
+++ js-node/testing/e2e/src/todo/api.js
@@ -1,6 +1,29 @@
 const http = require('http');
 
+const todos = [];
+
 const routes = {
+    "/todos:get": (request, response) => {
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify(todos));
+    },
+    "/todos:post": async (request, response) => {
+        let body = '';
+        for await (const chunk of request) {
+            body += chunk;
+        }
+        const todo = JSON.parse(body);
+        todo.id = 1;
+        todos.push(todo);
+        response.writeHead(201, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify(todo));
+    },
+    "/todos?id=1:get": async (request, response) => {
+        const { id } = request.params;
+        const todo = todos.find(t => t.id === id);
+        response.writeHead(200), { 'Content-Type': 'application/json' }
+        response.end(JSON.stringify(todo))
+    },
     default: (request, response) => {
         response.write('Hello World!');
         return response.end();
--- js-node/testing/e2e/src/todo/todo.test.js
+++ js-node/testing/e2e/src/todo/todo.test.js
@@ -14,4 +14,46 @@ describe('API Suite test', () => {
             assert.deepStrictEqual(response.text, 'Hello World!');
         })
     })
+
+    describe('/todos', () => {
+        it('Should request list todos is empty', async () => {
+            const todos = [];
+            const response = await request(app)
+                .get('/todos')
+                .expect(200);
+            
+            assert.deepStrictEqual(response.body, todos);
+        })
+
+        it('Should request create todo', async () => {
+            const todo = { title: 'New Task', status: 'pending' };
+            const response = await request(app)
+                .post('/todos')
+                .send(todo)
+                .expect(201);
+
+            assert.deepStrictEqual(response.body.title, todo.title);
+            assert.deepStrictEqual(response.body.status, todo.status);
+        })
+
+        it('Should request list tasks', async () => {
+            const todos = [{ id: 1, title: 'New Task', status: 'pending' }];
+            const response = await request(app)
+                .get('/todos')
+                .expect(200);            
+            
+            assert.deepStrictEqual(response.body, todos);
+        })
+
+        it('Should request todo By ID', async () => {
+            const todo = { id: 1, title: 'New Task', status: 'pending' };
+            const response = await request(app)
+                .get('/todos?id=1')
+                expect(200);
+
+            assert.deepStrictEqual(response.body.id, todo.id);
+            assert.deepStrictEqual(response.body.title, todo.title);
+            assert.deepStrictEqual(response.body.status, todo.status);
+        })
+    })
 })
