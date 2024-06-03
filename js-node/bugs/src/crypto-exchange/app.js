5a60fc0c2e483d220ea0a6555248c44ce0f2f861
igregorioneto <igregoriodoneto@outlook.com>
2024-06-03 18:13:34 -0300

    feat: Implementando o método de criar ordem e checar as ordenss

--- js-node/bugs/src/crypto-exchange/app.js
+++ js-node/bugs/src/crypto-exchange/app.js
@@ -2,17 +2,26 @@ const CryptoExchange = require('./cryptoExchange');
 
 const exchange = new CryptoExchange();
 
-const buyOrder = exchange.createOrder("buy", 1, 50000);
-const sellOrder = exchange.createOrder("sell", 0.5, 55000);
+exchange.createOrder("buy", 1, 50000);
+exchange.createOrder("sell", 0.5, 55000);
+exchange.createOrder("buy", 1, 50000);
+exchange.createOrder("sell", 0.5, 55000);
 
-buyOrder().then(result => {
-    console.log(`Buy order executed: ${result}`);
+const checkBuyOrders = exchange.checkOrderStatus('buy');
+const checkSellOrders = exchange.checkOrderStatus('sell');
+
+checkBuyOrders().then(result => {
+    for (const iterator of result) {
+        console.log(`Buy order executed: ${JSON.stringify(iterator)}`);
+    }  
 }).catch(err => {
     console.log(`Buy order execution failed: ${err}`);
 });
 
-sellOrder().then(result => {
-    console.log(`Sell order executed: ${result}`);
+checkSellOrders().then(result => {
+    for (const iterator of result) {
+        console.log(`Sell order executed: ${JSON.stringify(iterator)}`);
+    }    
 }).catch(err => {
     console.log(`Sell order execution failed: ${err}`);
 });
--- js-node/bugs/src/crypto-exchange/cryptoExchange.js
+++ js-node/bugs/src/crypto-exchange/cryptoExchange.js
@@ -1,3 +1,4 @@
+const Order = require('./order');
 class CryptoExchange {
     constructor() {
         this.orders = [];
@@ -6,17 +7,25 @@ class CryptoExchange {
     createOrder(type, amount, price) {
         const newOrder = new Order(this.orders.length + 1, type, amount, price);
         this.orders.push(newOrder);
+    }
 
-        // Closure para verificar o estado do pedido
-        const checkOrderStatus = () => {
+    // Closure para verificar o estado do pedido
+    checkOrderStatus(type)  {
+        return () => {
             return new Promise((resolve, reject) => {
                 // Implementar verificação assíncrona do pedido
                 // Simular atraso com setTimeout e resolver a promise com base na lógica de execução do pedido
+                setTimeout(() => {
+                    const ordersForType = this.orders.filter(o => o.type === type);
+                    if (ordersForType.length > 0) {
+                        resolve(ordersForType);
+                    } else {
+                        reject('No orders found for the given type.')
+                    }
+                }, 1000);
             });
-        };
-
-        return checkOrderStatus;
-    }
+        }        
+    };
 }
 
 module.exports = CryptoExchange;
