182e05116b53737a1f5f4fad8746126e29ad692a
igregorioneto <igregoriodoneto@outlook.com>
2024-06-11 22:10:03 -0300

    feat: iniciando os testes dos votos

--- js-node/bugs/src/poll/app.js
+++ js-node/bugs/src/poll/app.js
@@ -8,14 +8,26 @@ class Poll {
 
     vote(optionIndex, userId) {
         // Implemente a lógica para registrar o voto de um usuário para uma opção específica
+        this.votes.set(this.options[optionIndex], userId);
     }
 
     getTotalVotes() {
         // Implemente a lógica para retornar o número total de votos em todas as opções
+        console.log(this.votes.size())
+        return this.votes.size();
     }
 
     getVotesPerOption() {
         // Implemente a lógica para retornar um mapa com o número de votos para cada opção
+        let result = new Map();
+        for (const option of this.options) {
+            if (!result.has(option)) {
+                result.set(option, 1);
+            } else {
+                result.set(option, result.get(option) + 1);
+            }
+        }
+        return result;
     }
 
     getVotePercentage(optionIndex) {
@@ -37,10 +49,19 @@ class PollingApp {
 
     getPollById(pollId) {
         // Implemente a lógica para retornar uma enquete pelo seu ID
+        const poll = this.polls.find(p => p.id === pollId);
+        if (poll) {
+            return poll;
+        }
     }
 
     voteInPoll(pollId, optionIndex, userId) {
         // Implemente a lógica para registrar o voto de um usuário em uma enquete específica
+        const poll = this.getPollById(pollId);
+        if (optionIndex > poll.length - 1) {
+            return;
+        }
+        poll.vote(optionIndex, userId);
     }
 
     listPollResults(pollId) {
