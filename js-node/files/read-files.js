c28f51262c275d5383b48082c5bc08b4521834ef
igregorioneto <igregoriodoneto@outlook.com>
2024-06-21 20:52:03 -0300

    feat: arquivos

---
+++ js-node/files/read-files.js
@@ -0,0 +1,11 @@
+const fs = required('fs');
+
+;(() => {
+    // Criar uma pasta vazia
+
+    // Criar um arquivo na pasta
+
+    // Escrever um texto na pasta
+
+    // Ler o texto do arquivo
+})();
