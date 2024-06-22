f6aa264aaa9e533295fd916f1651b4d39a3e72be
igregorioneto <igregoriodoneto@outlook.com>
2024-06-21 21:51:07 -0300

    feat: implementando a conversa entre arquivos.

--- js-node/files/read-files.js
+++ js-node/files/read-files.js
@@ -1,11 +1,31 @@
-const fs = required('fs');
+const fs = require('fs');
 
 ;(() => {
     // Criar uma pasta vazia
+    let isDirectory = false
+    fs.readdir('./files/', { recursive: true } ,(err, files) => {
+        for (const file of files) {
+            if (file === 'tmp') {
+                isDirectory = true
+                console.log('Diretório tmp já existe!')
+                break;
+            }
+        }
+    })
+
+    if (!isDirectory) {
+        fs.mkdir('./files/tmp', { recursive: true } , (err, files) => {
+            console.log('Diretório tmp criado com sucesso!')
+        })
+    }
 
     // Criar um arquivo na pasta
-
-    // Escrever um texto na pasta
+    fs.writeFile('./files/tmp/arquivo.txt', 'Hello World!',  (err) => {
+        console.log('Arquivo criado com sucesso...')
+    })
 
     // Ler o texto do arquivo
+    fs.readFile('./files/tmp/arquivo.txt', 'utf-8', (err, dados) => {
+        console.log('Conteúdo do arquivo', dados)
+    })
 })();
