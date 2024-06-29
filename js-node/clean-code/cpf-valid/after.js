aff0fc09f3f57a208b7f7d51f62f6eeb0d7c370e
igregorioneto <igregoriodoneto@outlook.com>
2024-06-29 20:35:41 -0300

    feat: continuando a refatoração do cpf

--- js-node/clean-code/cpf-valid/after.js
+++ js-node/clean-code/cpf-valid/after.js
@@ -1,47 +1,46 @@
-function validate(str) {
+function validate(cpfEntrada) {
+    const validandoTipoEntrada = (cpfEntrada !== null && cpfEntrada !== undefined);
+    const validandoTamanhoEntrada = (cpfEntrada?.length >= 11 || cpfEntrada?.length <= 14);
 
-    if (str !== null && str !== undefined) {
-        if (str.length >= 11 || str.length <= 14) {
-            str = str.replace('.', '').replace('.', '').replace('-', '').replace(" ", "");
-            if (!str.split("").every(c => c === str[0])) {
-                try {
-                    let d1, d2;
-                    let dg1, dg2, rest;
-                    let digito;
-                    let nDigResult;
-                    d1 = d2 = 0;
-                    dg1 = dg2 = rest = 0;
+    if (validandoTipoEntrada && validandoTamanhoEntrada) {
+        cpfEntrada = transformandoCPFemArray(cpfEntrada);
+        if (validandoTodosOsValoresSaoIguais(cpfEntrada)) return false;
+        try {
+            let d1, d2;
+            let dg1, dg2, rest;
+            let digito;
+            let nDigResult;
+            d1 = d2 = 0;
+            dg1 = dg2 = rest = 0;
 
-                    for (let nCount = 1; nCount < str.length - 1; nCount++) {
-                        // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                        // 	return false;
-                        // } else {
+            for (let nCount = 1; nCount < cpfEntrada.length - 1; nCount++) {
+                digito = parseInt(cpfEntrada.substring(nCount - 1, nCount));
+                d1 = d1 + (11 - nCount) * digito;
+                d2 = d2 + (12 - nCount) * digito;
+            };
 
-                        digito = parseInt(str.substring(nCount - 1, nCount));
-                        d1 = d1 + (11 - nCount) * digito;
-                        d2 = d2 + (12 - nCount) * digito;
-                        // }
-                    };
+            rest = (d1 % 11);
+            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+            d2 += 2 * dg1;
+            rest = (d2 % 11);
+            if (rest < 2)
+                dg2 = 0;
+            else
+                dg2 = 11 - rest;
+            let nDigVerific = cpfEntrada.substring(cpfEntrada.length - 2, cpfEntrada.length);
+            nDigResult = "" + dg1 + "" + dg2;
+            return nDigVerific == nDigResult;
+        } catch (e) {
+            console.error("Erro ao validar CPF: " + e);
+            return false;
+        }
 
-                    rest = (d1 % 11);
-                    dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-                    d2 += 2 * dg1;
-                    rest = (d2 % 11);
-                    if (rest < 2)
-                        dg2 = 0;
-                    else
-                        dg2 = 11 - rest;
-                    let nDigVerific = str.substring(str.length - 2, str.length);
-                    nDigResult = "" + dg1 + "" + dg2;
-                    return nDigVerific == nDigResult;
-                } catch (e) {
-                    console.error("Erro !" + e);
-                    return false;
-                }
-            } else return false
-        } else return false;
-    } else return false;
+    }
+    return false;
 }
+
+const validandoTodosOsValoresSaoIguais = (cpf) => cpf.split("").every(c => c === cpf[0]);
+const transformandoCPFemArray = (cpf) => cpf.replaceAll(/[.-]/g, '').trim()
 
 module.exports = {
     validate
--- js-node/clean-code/cpf-valid/app.test.js
+++ js-node/clean-code/cpf-valid/app.test.js
@@ -1,51 +1,51 @@
-const before = require('./before');
+const cpf = require('./after');
 
 test("Deve validar o CPF A", function() {
-    const isValid = before.validate("935.411.347-80");
+    const isValid = cpf.validate("935.411.347-80");
     expect(isValid).toBeTruthy();
 })
 
 test("Deve validar o CPF B", function() {
-    const isValid = before.validate("357.188.378-05");
+    const isValid = cpf.validate("357.188.378-05");
     expect(isValid).toBeTruthy();
 })
 
 test("Deve validar o CPF C", function() {
-    const isValid = before.validate("987.654.321-00");
+    const isValid = cpf.validate("987.654.321-00");
     expect(isValid).toBeTruthy();
 })
 
 test("Não deve validar o CPF", function() {
-    const isValid = before.validate("111.111.111-11");
+    const isValid = cpf.validate("111.111.111-11");
     expect(isValid).toBeFalsy();
 })
 
 test("Não deve validar o CPF (Todos os números iguais)", function() {
-    const isValid = before.validate("111.111.111-11");
+    const isValid = cpf.validate("111.111.111-11");
     expect(isValid).toBeFalsy();
 })
 
 test("Não deve validar o CPF (número aleatório)", function() {
-    const isValid = before.validate("123.456.789-00");
+    const isValid = cpf.validate("123.456.789-00");
     expect(isValid).toBeFalsy();
 })
 
 test("Não deve validar o CPF (Além do limite)", function() {
-    const isValid = before.validate("123.456.789-00000000000");
+    const isValid = cpf.validate("123.456.789-00000000000");
     expect(isValid).toBeFalsy();
 })
 
 test("Não deve validar o CPF (Abaixo do limite)", function() {
-    const isValid = before.validate("123.456.789");
+    const isValid = cpf.validate("123.456.789");
     expect(isValid).toBeFalsy();
 })
 
 test("Não deve validar o CPF (Valor null)", function() {
-    const isValid = before.validate(null);
+    const isValid = cpf.validate(null);
     expect(isValid).toBeFalsy();
 })
 
 test("Não deve validar o CPF (Valor undefined)", function() {
-    const isValid = before.validate(undefined);
+    const isValid = cpf.validate(undefined);
     expect(isValid).toBeFalsy();
 })
