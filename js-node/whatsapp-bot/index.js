dd1b6750f47d76bb8c8b7975120fa476ef4591b5
igregorioneto <igregoriodoneto@outlook.com>
2024-07-12 19:37:40 -0300

    feat: carregando mensagens

--- js-node/whatsapp-bot/index.js
+++ js-node/whatsapp-bot/index.js
@@ -6,7 +6,7 @@ const wwebVersion = '2.2407.3';
 const client = new Client({
     authStrategy: new LocalAuth(), // your authstrategy here
     puppeteer: {
-      headless: true , args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
     webVersionCache: {
         type: 'remote',
@@ -23,16 +23,38 @@ client.on('message_create', message => {
     console.log(message.body);
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('Cliente está pronto!');
+    getChat();
 });
 
 client.on('message', (message) => {
-    console.log(message.body)
+    displayMessage(message.fromMe ? 'Eu' : message.from, message.body);
     if (message.body.startsWith('!ping')) {
         message.reply('pong');
         client.sendMessage(message.from, 'Olá!');
     }
 });
+
+// Função para formatar e exibir mensagens
+function displayMessage(chatName, messageBody) {
+    console.log(`[${chatName}] ${messageBody}`);
+}
+
+async function getChat() {
+    // Obtendo todas as conversas
+    const chats = await client.getChats();
+
+    for (const chat of chats) {
+        console.log(`Carregando mensagens da conversa: ${chat.name}`);
+
+        // Obtendo mensagens da conversa
+        const messages = await chat.fetchMessages({ limit: 100 }); // Limite de mensagens por vez
+
+        for (const message of messages) {
+            displayMessage(`[${chat.name}] ${message.body}`);
+        }
+    }
+}
 
 client.initialize();
