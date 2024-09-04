c7aaa7d4611c2ca1c63920d65f95fc79d2e757d7
igregorioneto <igregoriodoneto@outlook.com>
2024-09-04 20:03:38 -0300

    feat: Stack

--- datastructures-algorithms/examples/stacks/Stack.js
+++ datastructures-algorithms/examples/stacks/Stack.js
@@ -5,3 +5,6 @@ stack.push(5);
 stack.push(8);
 console.log(stack.items);
 console.log(stack.toString());
+console.log(Object.getOwnPropertyNames(stack));
+console.log(Object.keys(stack));
+console.log(stack.items);
