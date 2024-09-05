303eed401e851c70f34c888f5b77049db036e714
igregorioneto <igregoriodoneto@outlook.com>
2024-09-05 19:36:32 -0300

    feat: Stack

---
+++ datastructures-algorithms/examples/stacks/baseConverterExample.js
@@ -0,0 +1,6 @@
+import { baseConverter } from "./baseConverter.js";
+
+console.log(baseConverter(100345, 2));
+console.log(baseConverter(100345, 8));
+console.log(baseConverter(100345, 16));
+console.log(baseConverter(100345, 35));
