e8dcd5e0096ed542304385c85d39c25ecc66790f
igregorioneto <igregoriodoneto@outlook.com>
2024-09-24 19:33:25 -0300

    feat: linked list

--- datastructures-algorithms/src/js/doubly-linked-list/doubly-linked-list.js
+++ datastructures-algorithms/src/js/doubly-linked-list/doubly-linked-list.js
@@ -39,4 +39,30 @@ export class DoublyLinkedList extends LinkedList {
     }
     return false;
   }
+
+  removeAt(position) {
+    if (position >= 0 && position < this.count) {
+      let current = this.head;
+      if (position === 0) {
+        this.head = current.next;
+        if (this.count === 1) {
+          this.tail = undefined;
+        } else {
+          this.head.prev = undefined;
+        }
+      } else if (position === this.count - 1) {
+        current = this.tail;
+        this.tail = current.prev;
+        this.tail.next = undefined;
+      } else {
+        current = this.getElementAt(position);
+        const previous = current.prev;
+        previous.next = current.next;
+        current.next.prev = previous;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
 }
